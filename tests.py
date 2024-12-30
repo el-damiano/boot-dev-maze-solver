ea510daedfa00a8e3b197fd122c0791d0fccfed5
el-damiano <el9damiano@gmail.com>
2024-12-30 14:44:10 +0100

    test: maze entrance and exit walls broken

--- tests.py
+++ tests.py
@@ -29,6 +29,18 @@ class Tests(unittest.TestCase):
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(len(m1._cells), 0)
 
+    def test_maze_entrance_and_exit_walls_are_broken(self):
+        num_rows = 10
+        num_cols = 12
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+
+        maze_entrance = m1._cells[0][0]
+        maze_exit = m1._cells[m1._num_rows - 1 ][m1._num_cols - 1]
+        maze_entrance.has_left_wall == False
+        maze_exit.has_bottom_wall == False
+        self.assertFalse(maze_entrance.has_left_wall)
+        self.assertFalse(maze_exit.has_bottom_wall)
+
 
 if __name__ == "__main__":
     unittest.main()
